03a7d4538a927eb7b2aef1ce44ca9dc66211c461
Wiktor <wiktor.ludwiniak@gmail.com>
2021-06-12 17:02:09 +0200

    Create relationship project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShortenModule } from './shorten/shorten.module';
 import { UserModule } from './user/user.module';
+import { StatsModule } from './stats/stats.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [ShortenModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ShortenModule,
+    UserModule,
+    StatsModule,
+    RolesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
